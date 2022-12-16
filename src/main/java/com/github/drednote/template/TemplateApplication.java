d27b43a814d3012635f26dd8e4d913a9b8af6c9f
Drednote <galushko.ivan8@gmail.com>
2022-12-16 20:21:45 +0300

    initial commit

---
+++ src/main/java/com/github/drednote/template/TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.drednote.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TemplateApplication.class, args);
+  }
+
+}
